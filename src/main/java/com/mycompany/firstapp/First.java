6811619e5877037de8adf66ce83b49a125ac0619
Sheroz <Sheroz@MUBARRA-PC>
2021-08-04 08:14:40 +0530

    master commit

--- src/main/java/com/mycompany/firstapp/First.java
+++ src/main/java/com/mycompany/firstapp/First.java
@@ -11,6 +11,6 @@ package com.mycompany.firstapp;
  */
 public class First {
     public static void main(String[] args) {
-        System.out.println("Test branch");
+        System.out.println("Master branch");
     }
 }
